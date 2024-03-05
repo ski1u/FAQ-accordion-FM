35d57e2c9c7830b1163edc4e104585dfb1554864
steven <111696027+ski1u@users.noreply.github.com>
2024-03-04 19:19:45 -0500

    Update script.js
    file paths and details

--- script.js
+++ script.js
@@ -4,41 +4,41 @@ let text = document.getElementsByClassName("q")
 let amount = Math.max(plusicon.length, text.length)
 
 let icons = {
-    plus: "assets/images/icon-plus.svg",
-    minus: "assets/images/icon-minus.svg"
+    plus: "icon-plus.svg",
+    minus: "icon-minus.svg"
 }
 let descriptions = {
-    refundsandreturns: "All products are uniquely made to order at PODpartner. Therefore, we do not support returns or exchanges if you/your customer ordered the wrong size/color, or provided incorrect shipping details. You are responsible for capturing the correct information from your customers.",
-    shippingandhandling: "Customers will be notified when their order ships out. The tracking number will be automatically synced to your storefront so that customers can be updated on the shipping status.",
+    refundsandreturns: "We strive to ensure that you are completely satisfied with your purchase. If, for any reason, you are not satisfied with your purchase, we offer a straightforward refund and return policy to make your shopping experience hassle-free.",
+    shippingandhandling: " For eligible returns initiated within a week of purchase or delivery, customers can expect refunds issued promptly in the original form of payment or as store credit if necessary. While customers are responsible for return shipping costs, we cover such expenses for damaged, defective, or incorrect items.",
     privacyandownership: "The types of personal information we obtain about you depends on how you interact with our Site and use our Services. When we use the term personal information, we are referring to information that identifies, relates to, describes or can be associated with you. The following sections describe the categories and specific types of personal information we collect.",
-    termsofservice: "This website is operated by Ascend. Throughout the site, the terms “we”, “us” and “our” refer to Ascend. Ascend offers this website, including all information, tools and Services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here."
+    termsofservice: "In these terms of service, Your User Content shall mean any audio, video, text, images, or other material you choose to display on this website. By displaying Your User Content, you grant this business a non-exclusive, worldwide irrevocable, sub-licensable license to use, reproduce, adapt, publish, translate, and distribute it in any and all media."
 }
 
 for (let i = 0; i<amount; i++) {
-    text[i].addEventListener("click", function(event) { //clicking the text
+    text[i].addEventListener("click", function(event) {
         let element = event.target
         let svg = element.parentNode.querySelector("img")
 
         let desc = element.parentNode.querySelector(".desc")
-        let find = descriptions[element.innerHTML.split(" ").join("").toLowerCase()] //finds descripton by title name
+        let find = descriptions[element.innerHTML.split(" ").join("").toLowerCase()]
 
-        let card = document.querySelector(".card") //changes height based off of new description
+        let card = document.querySelector(".card")
         let currentheight = card.clientHeight
         let prevheight = 0
         let offset = 50
-        if (svg.src.endsWith(icons.plus)) { //changes img
+        if (svg.src.endsWith(icons.plus)) {
             svg.src = icons.minus
 
-            desc.innerHTML = find //sets description
+            desc.innerHTML = find
 
-            card.style.height = ((currentheight + desc.clientHeight) + offset) + "px" //formula for calculating the height once text put
-        } else {//resets
+            card.style.height = ((currentheight + desc.clientHeight) + offset) + "px"
+        } else {
             svg.src = icons.plus
 
             prevheight = desc.clientHeight
             desc.innerHTML = ""
             
-            card.style.height = ((currentheight - prevheight) - offset) + "px" //removing the height
+            card.style.height = ((currentheight - prevheight) - offset) + "px"
         }
     })
 }
